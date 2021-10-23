9176da81f7be636ef25555c2f535b855f67a0503
Shakil Ansary <shakilansary9@gmail.com>
2021-10-23 21:05:21 +0530

    JS updated

---
+++ dynamic.js
@@ -0,0 +1,25 @@
+let [miliseconds, seconds, minutes, hours];
+let timerRef = document.querySelector('.timerDisplay');
+let int;
+
+document.getElementById('.startTimer').addEventListener('click', ()=>{
+    int = setInterval(displayTimer, 10);
+});
+
+function displayTimer(){
+    miliseconds+=10;
+    if(miliseconds == 1000){
+        miliseconds = 0;
+        seconds++;
+        if(seconds == 60){
+            seconds = 0;
+            minutes++
+            if(minutes == 60){
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+} 
